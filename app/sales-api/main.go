392055e12eaaaeae3ee59d180e5ac81e60e866bb
William Kennedy <bill@ardanlabs.com>
2021-08-03 13:39:26 -0400

    Day2 Push17

--- app/sales-api/main.go
+++ app/sales-api/main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
+	"time"
 
+	"github.com/ardanlabs/conf"
 	"github.com/ardanlabs/service/foundation/logger"
+	"github.com/pkg/errors"
+	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
 )
 
 // build is the git version of this program. It is set using build flags in the makefile.
@@ -19,4 +25,65 @@ func main() {
 		os.Exit(1)
 	}
 	defer log.Sync()
+
+	// Perform the startup and shutdown sequence.
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+
+	// =========================================================================
+	// CPU Quota
+
+	// Make sure the program is using the correct
+	// number of threads if a CPU quota is set.
+	if _, err := maxprocs.Set(); err != nil {
+		log.Errorw("startup", zap.Error(err))
+		os.Exit(1)
+	}
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// =========================================================================
+	// Configuration
+
+	cfg := struct {
+		conf.Version
+		Web struct {
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+		}
+	}{
+		Version: conf.Version{
+			SVN:  build,
+			Desc: "copyright information here",
+		},
+	}
+
+	if err := conf.Parse(os.Args[1:], "SALES", &cfg); err != nil {
+		switch err {
+		case conf.ErrHelpWanted:
+			usage, err := conf.Usage("SALES", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config usage")
+			}
+			fmt.Println(usage)
+			return nil
+		case conf.ErrVersionWanted:
+			version, err := conf.VersionString("SALES", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating config version")
+			}
+			fmt.Println(version)
+			return nil
+		}
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/ardanlabs/service
 
 go 1.16
 
-require go.uber.org/zap v1.18.1
+require (
+	github.com/ardanlabs/conf v1.4.0
+	github.com/pkg/errors v0.8.1
+	go.uber.org/automaxprocs v1.4.0
+	go.uber.org/zap v1.18.1
+)
